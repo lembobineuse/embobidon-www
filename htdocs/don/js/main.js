abb034151ad53bf8366996ce2ea050f3e899fd48
ju1ius <jules.bernable@laposte.net>
2015-01-29 03:51:19 +0100

    Use jQuery.Fancybox correctly

--- htdocs/don/js/main.js
+++ htdocs/don/js/main.js
@@ -4,21 +4,12 @@
 // DOMContentLoaded handler
 $(function()
 {
-    $('.fancybox').fancybox();
-    $('.fancy_vimeo').click(function() {
-        $.fancybox({
-            padding: 0,
-            autoScale: false,
-            transitionIn: 'none',
-            transitionOut: 'none',
-            title: this.title,
-            width: 800,
-            height: 500,
-            href: 'http://player.vimeo.com/video/' + this.getAttribute('data-video-id') + '?autoplay=1',
-            type: 'iframe'
-        });
-        return false;
+    $('.fancybox').fancybox({
+        padding: 0,
+        helpers : {
+            media : {}
+        }
     });
 });
-    
+
 }(this, this.jQuery));
