2d1ea2df3fb0f99c576ec5e125c0f31e6203e959
ju1ius <jules.bernable@laposte.net>
2015-02-01 11:47:34 +0100

    prevent clear-cache script from deleting cache directory

--- don/bin/clear-cache.sh
+++ don/bin/clear-cache.sh
@@ -5,6 +5,6 @@ __DIR__=$(dirname "$__FILE__")
 
 CACHE_DIR="$__DIR__/../cache"
 
-find "$CACHE_DIR" -type d -print0 | xargs -0 rm -rf
+find "$CACHE_DIR" -mindepth 1 -type d -print0 | xargs -0 rm -rf
 
 exit 0
