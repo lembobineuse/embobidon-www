343d048120f3f39af0156b0fccfad5507d3c0a84
ju1ius <jules.bernable@laposte.net>
2015-02-02 07:20:11 +0100

    Modified static assets layout & gulp tasks

--- don/Gulpfile.js
+++ don/Gulpfile.js
@@ -5,26 +5,31 @@ var gulp = require('gulp'),
     minifycss = require('gulp-minify-css'),
     autoprefixer = require('gulp-autoprefixer'),
 
-    web_dir = '../htdocs/don',
     scripts = [
-        web_dir + '/js/embobidon/image-wall.js',
-        web_dir + '/js/main.js'
+        '../htdocs/don/js/src/image-wall.js',
+        '../htdocs/don/js/src/main.js'
     ],
     styles = [
-        web_dir + '/css/main.css'
+        '../htdocs/don/css/src/main.css'
     ]
 ;
 
-gulp.task('default', ['styles', 'scripts']);
+gulp.task('default', ['watch', 'styles', 'scripts']);
 
-gulp.task('scripts', function()
+gulp.task('watch', function ()
+{
+    gulp.watch('../htdocs/don/js/src/**', ['scripts']);
+    gulp.watch('../htdocs/don/css/src/**', ['styles']);
+});
+
+gulp.task('scripts', function ()
 {
     return gulp.src(scripts)
         .pipe(concat('all.js'))
         // This will minify and rename to foo.min.js
         .pipe(uglify())
         .pipe(rename({ extname: '.min.js' }))
-        .pipe(gulp.dest(web_dir + '/js/'))
+        .pipe(gulp.dest('../htdocs/don/js/dist'))
     ;
 });
 
@@ -35,6 +40,6 @@ gulp.task('styles', function ()
         .pipe(concat('all.css'))
         .pipe(minifycss())
         .pipe(rename({ extname: '.min.css'} ))
-        .pipe(gulp.dest(web_dir + '/css/'))
+        .pipe(gulp.dest('../htdocs/don/css/dist'))
     ;
 });
