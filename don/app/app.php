6ad94ddd80e0dec6ad4f5c204bdce0389cab6680
ju1ius <jules.bernable@laposte.net>
2015-02-03 18:32:17 +0100

    Moved cache & logs directory to /don/var

--- don/app/app.php
+++ don/app/app.php
@@ -9,8 +9,8 @@ $app['app_dir'] = __DIR__;
 $app['config_dir'] = __DIR__.'/config';
 $app['root_dir'] = __DIR__.'/../..';
 
-$app['cache_dir'] = __DIR__.'/cache';
-$app['log_dir'] = __DIR__.'/logs';
+$app['cache_dir'] = __DIR__.'/../var/cache';
+$app['log_dir'] = __DIR__.'/../var/logs';
 $app['tpl_dir'] = __DIR__.'/resources/views';
 
 $app['web_dir'] = $app['root_dir'].'/htdocs/don';
